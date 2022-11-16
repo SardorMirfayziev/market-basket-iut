b51e2a1d16fccabf59ffec19f96b53638f75c7a4
Sardor <sardormirfayziev@gmail.com>
2022-11-16 17:37:45 +0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { Button, message, Upload, Form, Input, Checkbox } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+
+const mainUrl = "https://54.165.164.131";
 
 function App() {
+  const handleChangeFile = (file) => {
+    console.log(file);
+  };
+
+  const onSubmit = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.mainTitle}>Market basket analysis</h1>
+
+        <div className={styles.uploads}>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            layout="vertical"
+            wrapperCol={{
+              span: 16,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            autoComplete="off"
+          >
+            <Form.Item label="Minimum support" name="username">
+              <Input />
+            </Form.Item>
+
+            <Form.Item label="something" name="password">
+              <Input />
+            </Form.Item>
+
+            <Form.Item name="remember" valuePropName="checked">
+              <Upload onChange={(e) => handleChangeFile(e)}>
+                <Button icon={<UploadOutlined />}>Click to Upload</Button>
+              </Upload>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 }
