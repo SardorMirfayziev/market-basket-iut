9a63c2f94f88d8bf7747071d286b0ae7bae63433
Sardor <sardormirfayziev@gmail.com>
2022-11-16 18:56:21 +0500

    wip-7

--- src/App.js
+++ src/App.js
@@ -51,8 +51,74 @@ function App() {
           <button type="button">Submit</button>
         </div>
       </form>
+
+      <div
+        style={{
+          padding: "20px",
+          border: "1px solid gold",
+          marginTop: "40px",
+        }}
+      >
+        <h2>Most Frequent tuples</h2>
+        {mock.map(([it1, it2], index) => (
+          <div
+            style={{
+              display: "flex",
+              marginBottom: "10px",
+            }}
+          >
+            <p
+              style={{
+                margin: "0 10px 0 0",
+              }}
+            >
+              {index})
+            </p>
+            <span>
+              {it1}, {it2}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
+const mock = [
+  ["whole milk", "tropical fruit"],
+  ["white bread", "ham"],
+  ["other vegetables", "tropical fruit"],
+  ["sugar", "flour"],
+  ["red/blush wine", "bottled beer"],
+  ["citrus fruit", "tropical fruit"],
+  ["tropical fruit", "whole milk"],
+  ["whipped/sour cream", "whole milk"],
+  ["processed cheese", "white bread"],
+  ["herbs", "whole milk"],
+  ["other vegetables", "herbs"],
+  ["other vegetables", "butter"],
+  ["whole milk", "curd"],
+  ["whole milk", "pip fruit"],
+  ["other vegetables", "pip fruit"],
+  ["bottled beer", "liquor"],
+  ["other vegetables", "pip fruit"],
+  ["tropical fruit", "other vegetables"],
+  ["yogurt", "whole milk"],
+  ["other vegetables", "pip fruit"],
+  ["other vegetables", "whole milk"],
+  ["other vegetables", "tropical fruit"],
+  ["whole milk", "butter"],
+  ["whole milk", "pip fruit"],
+  ["Instant food products", "hamburger meat"],
+  ["whole milk", "butter"],
+  ["citrus fruit", "whole milk"],
+  ["long life bakery product", "chocolate"],
+  ["long life bakery product", "chocolate"],
+  ["other vegetables", "butter"],
+  ["root vegetables", "butter"],
+  ["sausage", "root vegetables"],
+  ["citrus fruit", "pip fruit"],
+  ["other vegetables", "whole milk"],
+];
 
 export default App;
